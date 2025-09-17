d4f09a4fe3f3c36f8c68e3e2fa82abc34ba85b78
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-09-17 14:09:48 +0900

    호텔

--- src/main/java/com/jhj/hotel/SpringMiniprojectApplication.java
+++ src/main/java/com/jhj/hotel/SpringMiniprojectApplication.java
@@ -2,9 +2,19 @@ package com.jhj.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringMiniprojectApplication {
+public class SpringMiniprojectApplication extends SpringBootServletInitializer{
+
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMiniprojectApplication.class, args);
